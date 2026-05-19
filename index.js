db64d94a1436a8988421523fee4accc84a49563c
deongggg <deomacasero@gmail.com>
2026-05-19 13:53:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello from my CI/CD pipeline!");
+    return res.send("Hello World!");
 })
 
 app.get('/api', function (req, res) {
